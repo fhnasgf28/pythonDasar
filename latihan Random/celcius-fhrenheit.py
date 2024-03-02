e020984671b4444410a11ea9f818f3c34b6bbd55
fhnasgf28 <farhan@nibble.co.id>
2024-03-02 09:15:35 +0700

    Latihan Random
    Dibulan Maret tanggal 2

---
+++ latihan Random/celcius-fhrenheit.py
@@ -0,0 +1,7 @@
+# menginput suhu dalam derajat celcius
+
+celcius = float(input("Tuliskan Suhu dalam Celcius"))
+# menghitung suhu dalam derarjat fahrenheit
+fahrenheit = (celcius * 1.82) + 32
+# menampilkan hasil konversi
+print('%0.2f Derajat Celcius sama dengan %0.2f Derajat Fahrenheit' % (celcius, fahrenheit))
