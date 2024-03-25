0b2bf00d1820beece3e8e62d98139329cdee6711
fhnasgf28 <farhan.assegaf19019@student.unsika.ac.id>
2024-03-25 07:51:30 +0700

    [Latihan Random] Pencarian Data List

---
+++ debugging/program1.py
@@ -0,0 +1,20 @@
+# debugging program example
+
+def divide(n1, n2):
+    return n1 / n2
+
+def add(n1, n2):
+    return n1 + n2
+
+def substract(n1, n2):
+    return n1 - n2
+
+def multiply(n1, n2):
+    return n1 * n2
+
+
+n1 = int(input("Enter first number: "))
+n2 = int(input("Enter second number: "))
+
+result = divide(n1, n2)
+print(result)
