04b1e51fa535dc2b3544e4e1a731ab6cefb8908a
fhnasgf28 <farhan.assegaf19019@student.unsika.ac.id>
2024-03-31 03:10:57 +0700

    latihan 54_import

---
+++ 54_import/main_app.py
@@ -0,0 +1,18 @@
+
+# untuk menyambung program dari external
+import program_ucup
+import program_print
+
+# import dengan data
+import variabel
+import kucuy
+
+# data ada di namespace variabel
+print(variabel.data)
+print(kucuy.data)
+
+# import dengan fungsi
+import matematika
+
+hasil = matematika.tambah(4, 5)
+print(hasil)
---
+++ 54_import/variabel.py
@@ -0,0 +1,1 @@
+data = "Bulan penuh ampuban"
