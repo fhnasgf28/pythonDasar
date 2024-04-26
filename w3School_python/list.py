2beeada0cd78134b8ff10a9dd1768d9c82e3e054
fhnasgf28 <farhan.assegaf19019@student.unsika.ac.id>
2024-04-26 22:02:48 +0700

    Learn Everyday Python w3School

---
+++ w3School_python/list.py
@@ -0,0 +1,20 @@
+thislist = ["apple", "banana", "cherry", "apple", "cherry"]
+print(thislist)
+
+# allow duplicates
+# list length
+print(len(thislist))
+
+# List Items - Data Types
+list0 = ["apple", "banana", "cherry"]
+list1 = [3, 4, 5, 6, 7]
+list2 = [True, False, True]
+
+list4 = ["ABC", 34, True, 40, "male"]
+
+# type
+
+mylist = ["apple", "banana", "cherry"]
+print(type(mylist))
+
+# The list() Constructor
