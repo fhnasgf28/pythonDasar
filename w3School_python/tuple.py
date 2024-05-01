eaa50e4a7591f540e392bd908fb413172bc6f947
fhnasgf28 <farhan.assegaf19019@student.unsika.ac.id>
2024-05-01 21:04:25 +0700

    Learn Everyday Python w3School (Tuple)

---
+++ w3School_python/tuple.py
@@ -0,0 +1,21 @@
+# Python Tuples
+
+# tuple allow Duplicates
+thistuple = ("apple", "banana", "cherry", "apple", "cherry")
+print(thistuple)
+
+# tuple length
+print(len(thistuple))
+
+# Create Tuple With One Item
+
+# bukan tuple
+thistuple1 = ("apple",)
+print(type(thistuple1))
+
+# not a tuple
+thistuple2 = ("apple1")
+print(type(thistuple2))
+
+# tuple items - data types
+
