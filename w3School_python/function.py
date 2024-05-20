0d3257f4185049ca148935d78111a9f7d5a79ec6
Farhan Assegaf <farhan.assegaf19019@student.unsika.ac.id>
2024-05-20 15:23:15 +0700

    [ADD] SQL

--- w3School_python/function.py
+++ w3School_python/function.py
@@ -1,5 +1,5 @@
 def my_function(fname):
-    print(fname +' Hello from a function')
+    print(fname + ' Hello from a function')
 
     #calling a function
 
@@ -15,15 +15,19 @@ A parameter is the variable listed inside the parentheses in the function defini
 
 An argument is the value that is sent to the function when it is called.'''
 
+
 # number of arguments
 def my_function2(fname, lname):
     print(fname + ' ' + lname)
 
-my_function2('Farhan','assegaf')
+
+my_function2('Farhan', 'assegaf')
+
 
 # Arbitrary Arguments, *args
 def my_function3(*kids):
     print('the youngest child is' + kids[2])
+
 
 my_function3('emil', 'Tobias', 'Linus')
 
