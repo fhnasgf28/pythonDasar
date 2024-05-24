f84a8c7bdc385bc6130f240ba0afed0a03c9f5ac
Farhan Assegaf <farhan.assegaf19019@student.unsika.ac.id>
2024-05-24 08:37:21 +0700

    [ADD] Kelas Terbuka OOP

--- w3School_python/python_inheritance.py
+++ w3School_python/python_inheritance.py
@@ -62,3 +62,23 @@ class Student(Person5):
 
 x = Student("Mike", "Olsen", 2019)
 x.welcome()
+
+# class Hero
+
+class Hero:
+    def __init__(self, name, health):
+        self.name = name
+        self.health = health
+
+class Hero_intelligent(Hero):
+    pass
+class Hero_strength(Hero):
+    pass
+
+lina = Hero('lina', 100)
+techies = Hero_intelligent('techies',50)
+axe = Hero_strength('axe',200)
+
+print(lina.name)
+print(techies.name)
+print(axe.name)
