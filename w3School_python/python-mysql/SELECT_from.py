ecef28de470d134a9a70ca11aa5467894415c260
fhnasgf28 <farhan.assegaf19019@student.unsika.ac.id>
2024-06-02 17:38:14 +0700

    [ADD] latihan Python SQL SELECT

---
+++ w3School_python/python-mysql/SELECT_from.py
@@ -0,0 +1,15 @@
+import mysql.connector
+
+# create connection
+mydb = mysql.connector.connect(
+    host='localhost',
+    user='root',
+    database="mydatabase",
+)
+myCursor = mydb.cursor()
+
+myCursor.execute('SELECT * FROM customers')
+myresult = myCursor.fetchall()
+
+for x in myresult:
+    print(x)
