c3cb0718e424640711ac5c43c88d502319455543
fhnasgf <farhan.assegaf19019@student.unsika.ac.id>
2024-06-13 15:21:03 +0700

    [ADD] python Data Structure

---
+++ geekForGeek/operator.py
@@ -0,0 +1,28 @@
+a = 9
+b = 5
+add = a + b
+sub = a - b
+mul = a * b
+mod = a % b
+
+p = a ** b
+
+print(add)
+print(sub)
+print(mul)
+print(mod)
+print(p)
+
+print(a > b)
+print(a < b)
+print(a == b)
+print(a != b)
+print(a >= b)
+print(a <= b)
+
+print('==================')
+a = True
+b = False
+print(a and b)
+print(a or b)
+print(not a)
