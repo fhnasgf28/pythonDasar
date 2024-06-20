65d4c847468e69b6f1222e26b5ed3b7cf38ef7c0
fhnasgf <farhan.assegaf19019@student.unsika.ac.id>
2024-06-20 23:14:32 +0700

    [ADD] tuple geek for geek

---
+++ geekForGeek/1_find_the_size_tuple.py
@@ -0,0 +1,7 @@
+# sample tuples
+tuple1 = ("A", 1, "B", 2, "C", 3)
+tuple2 = ("Geek1", "Raju", "Geek2", "Nikhil", "Geek3", "Deepanshu")
+tuple3 = ((1, "Lion"), ( 2, "Tiger"), (3, "Fox"), (4, "Wolf"))
+
+# print the sizes of sample tuples
+print("size of tuple1:" + str(tuple3.__sizeof__()) + "bytes")
