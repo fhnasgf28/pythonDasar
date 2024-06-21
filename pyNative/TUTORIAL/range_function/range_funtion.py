d56d1f60b51f8f1667ad7c4cfad382e8652b6365
fhnasgf <farhan.assegaf19019@student.unsika.ac.id>
2024-06-21 08:36:09 +0700

    [ADD] PYNATIVE Python range_funtion.py

---
+++ pyNative/TUTORIAL/range_function/range_funtion.py
@@ -0,0 +1,11 @@
+for i in range(10):
+    print(i, end= ' ' )
+
+print('============================')
+# range(start, stop)
+for o in range(10, 16):
+    print(o, end=' ')
+print('\n============')
+# range(start, stop, step)
+for i in range(10, 50, 5):
+    print(i, end=' ')
