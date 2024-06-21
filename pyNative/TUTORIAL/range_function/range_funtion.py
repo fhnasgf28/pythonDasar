c358fd81ab321514839d36268fbbfc4139e6ce47
fhnasgf <farhan.assegaf19019@student.unsika.ac.id>
2024-06-21 09:11:06 +0700

    add pynative

--- pyNative/TUTORIAL/range_function/range_funtion.py
+++ pyNative/TUTORIAL/range_function/range_funtion.py
@@ -9,3 +9,17 @@ print('\n============')
 # range(start, stop, step)
 for i in range(10, 50, 5):
     print(i, end=' ')
+
+
+for h in range(9, 100, 3):
+    print(h, end=' ')
+
+# for loop with range()
+for i in range(1, 10, 2):
+    print("Current value of i is:", i)
+
+# iterate a list using range() and for loop
+list1 = ['Jessa', 'Emma', 20, 30, 75.5]
+
+for i in range(len(list1)):
+    print(list1[i])
