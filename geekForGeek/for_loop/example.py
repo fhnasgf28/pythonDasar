2fcfbdfcc8b36e1177ae5e6dfc9f7b4286bf1a05
fhnasgf28 <farhan.assegaf19019@student.unsika.ac.id>
2024-07-07 23:39:13 +0700

    [ADD] geeks for geeks for loop

--- geekForGeek/for_loop/example.py
+++ geekForGeek/for_loop/example.py
@@ -7,3 +7,8 @@ for item in clothes:
         print(f"washing {item}")
 paired_socks.append("socks")
 print(f"washing {paired_socks}")
+
+for day in range(1, 8):
+    distance = 3 + (day - 1) * 0.5
+    print(distance)
+    print(f"Day {day}: Run {distance: .1f} Miles")
