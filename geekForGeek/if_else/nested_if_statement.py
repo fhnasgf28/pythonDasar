6bbd1893a01701bb27a7dc816934fe9cbbfcde58
fhnasgf28 <farhan.assegaf19019@student.unsika.ac.id>
2024-07-07 23:32:19 +0700

    [ADD] geeks for geeks for loop

---
+++ geekForGeek/if_else/nested_if_statement.py
@@ -0,0 +1,24 @@
+i = 10
+if i == 10:
+
+    # first if statement
+    if i < 15:
+        print("i is smaller than 15")
+
+    if i < 12:
+        print("i is smaller than 12 too")
+    else:
+        print("i is greather than 15")
+
+g = 15
+if g == 10:
+    print("g is 10")
+elif g == 15:
+    print("g is 15")
+elif g == 30:
+    print("g is 30")
+else:
+    print("g is not present")
+
+g = 100
+print(True) if g > 90 else print(False)
