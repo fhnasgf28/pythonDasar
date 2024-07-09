a2e42aadc0d60c69093b8ffbaee7a3d6ac15c575
fhnasgf <farhan.assegaf19019@student.unsika.ac.id>
2024-07-09 19:47:26 +0700

    [ADD] perulangan for pada toko & add discount

--- latihan Random/gemini/toko.py
+++ latihan Random/gemini/toko.py
@@ -5,10 +5,16 @@ products = [
     {"name": "Komputer", "price": 6000000},
 ]
 
+discount_rate = 0.1
 total_price = 0
 for product in products:
     quantity = int(input(f"Masukan jumlah {product['name']:}"))
     item_price = product['price'] * quantity
+    discount_amount = item_price * discount_rate
+    discounted_price = item_price - discount_amount
     total_price += item_price
 
     print(f"{product['name']} x {quantity} = {item_price}")
+    if discount_amount > 0:
+        print(f"discon: {discount_amount}")
+        print(f"harga discount: {discounted_price}")
