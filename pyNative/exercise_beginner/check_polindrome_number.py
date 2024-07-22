1d29f86124c226ccedb6ffa4f621ea393670a287
fhnasgf <farhan.assegaf19019@student.unsika.ac.id>
2024-07-22 22:07:10 +0700

    [ADD] latihan pnative

---
+++ pyNative/exercise_beginner/check_polindrome_number.py
@@ -0,0 +1,20 @@
+def polindrome(number):
+    print("Original Number", number)
+    original_num = number
+
+    # reverse the given number
+    reverse_num = 0
+    while number > 0:
+        reminder = number % 10
+        reverse_num = (reverse_num * 10) + reminder
+        number = number // 10
+
+    # check numbers
+    if original_num == reverse_num:
+        print("Given number palindrome")
+    else:
+        print("Given Number is a palindrome")
+
+
+polindrome(121)
+polindrome(123)
---
+++ pyNative/exercise_beginner/remove_n.py
@@ -0,0 +1,9 @@
+def remove_chars(word, n):
+    print("Original string:", word)
+    x = word[n:]
+    return x
+
+
+print('Removing Character from a string')
+print(remove_chars("Pynative", 4))
+print(remove_chars("Pynative", 2))
