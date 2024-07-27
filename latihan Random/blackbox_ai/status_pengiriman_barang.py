c71fd620911c233be520cbef323d002b15a6cb9e
fhnasgf <farhan.assegaf19019@student.unsika.ac.id>
2024-07-27 07:43:46 +0700

    [UPDATE] latihan perulangan blackbox_ai

--- latihan Random/blackbox_ai/status_pengiriman_barang.py
+++ latihan Random/blackbox_ai/status_pengiriman_barang.py
@@ -5,6 +5,8 @@ status_pengiriman = {
     "4": "Pengiriman gagal",
 }
 
+data_pengiriman = {}
+
 
 def cek_status(no_resi):
     # simulasikan status pengiriman berdasarkan no resi
@@ -16,12 +18,44 @@ def cek_status(no_resi):
         return status_pengiriman['1']
 
 
+def tambah_pengiriman(no_resi, tanggal_pengiriman, estimasi_pengiriman, ):
+    #tambahkan data pengiriman ke kamus
+    data_pengiriman[no_resi] = {
+        "tanggal_pengiriman": tanggal_pengiriman,
+        "estimasi_waktu": estimasi_pengiriman,
+        "status_pengiriman": cek_status(no_resi)
+    }
+
+def tampilkan_pengiriman(no_resi):
+    #tampilkan data pengiriman
+    if no_resi in data_pengiriman:
+        print(f"No, Resi: {no_resi}")
+        print(f"Tanggal Pengiriman: {data_pengiriman[no_resi]['tanggal_pengiriman']}")
+        print(f"Estimasi Waktu Pengiriman: {data_pengiriman[no_resi]['estimasi_waktu']}")
+        print(f"Status Pengiriman: {data_pengiriman[no_resi]['status_pengiriman']}")
+    else:
+        print("No Resi tidak ditemukan")
+
+
 def main():
     print("Selamat datang di sistem pelacakan pengiriman")
-    no_resi = input("Masukkan nomor resi pengirimn: ")
-    status = cek_status(no_resi)
-    print(f"Status Pengiriman : {status}")
-
+    while True:
+        print("1. Tambah pengiriman")
+        print("2. Tampilkan pengiriman")
+        print("3. Keluar")
+        pilihan = input("Pilih Menu:/t")
+        if pilihan =='1':
+            no_resi = input("Masukkan nomor resi pengirimn: ")
+            tanggal_pengiriman = input("Masukkan tanggal pengiriman (YYY-MM-DD): ")
+            estimasi_pengiriman = input("Masukan estimasi waktu pengiriman (hari):")
+            tambah_pengiriman(no_resi, tanggal_pengiriman, estimasi_pengiriman)
+        elif pilihan =='2':
+            no_resi = input("Masukkan nomor resi pengiriman: ")
+            tampilkan_pengiriman(no_resi)
+        elif pilihan == '3':
+            break
+        else:
+            print("Pilihan tidak valid")
 
 if __name__ == "__main__":
     main()
