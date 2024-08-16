898b7e318be603a83300d736a5448343547164a4
fhnasgf <farhan.assegaf19019@student.unsika.ac.id>
2024-08-16 23:50:08 +0700

    [ADD] latihan penilaian_kinerja_karyawan.py chatGpt

---
+++ kelas dan Objek/chat-gpt/latihan.py
@@ -0,0 +1,8 @@
+def hitung_pangkat(bilangan, pangkat):
+  if pangkat > 1:
+    return bilangan * hitung_pangkat(bilangan, pangkat - 1)
+
+  return bilangan
+
+hasil = hitung_pangkat(bilangan, pangkat)
+print(f'Hasil = {hasil}')
