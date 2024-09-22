1d649c3e5bdc54d4d46f4b245f74d1639bdceff5
fhnasgf <farhan.assegaf19019@student.unsika.ac.id>
2024-09-22 23:09:41 +0700

    [ADD] latihan distance

---
+++ latihan_Random/blackbox-ai/calculate_distance.py
@@ -0,0 +1,10 @@
+def calculate_distance(speed, time):
+    distance = speed * time
+    return distance
+
+speed = float(input("Enter speed (km/h): "))
+time = float(input("Enter time(hours): "))
+
+# calculate and display the distance
+distance = calculate_distance(speed, time)
+print("The distance traveled is {: .2f}km.".format(distance))
