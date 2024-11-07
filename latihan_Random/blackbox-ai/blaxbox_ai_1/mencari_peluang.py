fcab577971640a36d6f816120a7350a313091580
fhnasgf28 <farhan.assegaf19019@student.unsika.ac.id>
2024-11-07 18:23:45 +0700

    [ADD] belum sesuai

---
+++ latihan_Random/blackbox-ai/blaxbox_ai_1/mencari_peluang.py
@@ -0,0 +1,16 @@
+# membuat list untuk mencari data mahasiswa
+mahasiswa = [
+    ("budi","Teknik Informatika", 90),
+    ("andi", "Teknik Informatika", 90),
+    ("siti","Teknik Informatika", 90),
+]
+
+# mencari data mahasiswa
+for mahasiswa in mahasiswa:
+    if mahasiswa[0] == "budi":
+        print(mahasiswa)
+
+# mencari data mahasiswa
+for mahasiswa in mahasiswa:
+    if mahasiswa[0] == "budi":
+        print(mahasiswa)
