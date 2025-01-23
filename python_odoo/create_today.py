48b83b7ce62d67c4d4668dd9a6e3da32a74567ec
fhnasgf28 <farhan.assegaf19019@student.unsika.ac.id>
2025-01-23 15:48:23 +0700

    create_today.py

--- python_odoo/create_today.py
+++ python_odoo/create_today.py
@@ -4,3 +4,12 @@ def _check_activity_today(self, activity):
         lead = self.env['crm.lead'].browse(activity.res_id)
         if lead:
             lead._get_leads_to_follow_up(time_range='day')
+
+    #
+    # def write(self, vals):
+    #     res = super(CalendarEvent, self).write(vals)
+    #     if 'start' in vals or 'res_model' in vals or 'res_id' in vals:
+    #         # self.env['crm.lead'].update_this_week_meeting()
+    #         self._cron_update_meeting_status()
+    #     return res
+
