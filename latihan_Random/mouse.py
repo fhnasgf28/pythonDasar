084817387934965b9aaed65e09d2f263a544d668
fhnasgf28 <farhan.assegaf19019@student.unsika.ac.id>
2025-05-23 22:10:50 +0700

    mouse update random

--- latihan_Random/mouse.py
+++ latihan_Random/mouse.py
@@ -1,15 +1,16 @@
 import pyautogui
 import time
+import random
 
 # Jarak gerakan mouse ke kanan (dalam pixel)
-offset = 100
+offset = random.randint(1, 100)
 
 try:
     while True:
         current_x, current_y = pyautogui.position()
-        new_x = current_x + offset
+        new_x = current_x + random.randint(1,1000)
         pyautogui.moveTo(new_x, current_y, duration=0.5)
         print(f"Mouse moved to: ({new_x}, {current_y})")
-        time.sleep(60)  # tunggu 1 menit
+        time.sleep(10)  # tunggu 1 menit
 except KeyboardInterrupt:
     print("Program dihentikan.")
