2901a635c136c17dcfae78f41d06ee301d34f52b
fhnasgf28 <farhan.assegaf19019@student.unsika.ac.id>
2025-06-01 22:16:04 +0700

    kodingan ini berhasil

--- python_odoo/pursales/action_confirm_jpl.py
+++ python_odoo/pursales/action_confirm_jpl.py
@@ -12,17 +12,42 @@ class CrmJourneyPlanFeedbackWizard(models.TransientModel):
             'feedback': self.feedback,
             'feedback_created_by': self.env.user.id
         })
-        activity_ids = []
-        activity_ids.extend(self.env['mail.activity'].search([('res_model_id', '=', self.env['ir.model'].search([('model', '=', 'crm.journey.plan.line')], limit=1).id),('res_id','=',self.jpl_id.id)]))
-        if self.jpl_id.opportunity_id:
-            activity_ids.extend(self.env['mail.activity'].search([('res_model_id', '=', self.env['ir.model'].search([('model', '=', 'crm.lead')], limit=1).id),('res_id','=',self.jpl_id.opportunity_id.id)]))
-        if activity_ids:
-            for activity_id in activity_ids:
-                activity_id.feedback = self.feedback
-                activity_id._action_done()
-                activity_id.write({
+
+        # Cari aktivitas spesifik untuk journey plan line yang terkait dengan activity_type_id
+        jpl_activity = False
+        if self.jpl_id.activity_type_id:
+            jpl_activity = self.env['mail.activity'].search([
+                ('res_model_id', '=',
+                 self.env['ir.model'].search([('model', '=', 'crm.journey.plan.line')], limit=1).id),
+                ('res_id', '=', self.jpl_id.id),
+                ('activity_type_id', '=', self.jpl_id.activity_type_id.id)
+            ], limit=1)
+
+        # Tandai aktivitas journey plan line sebagai selesai jika ditemukan
+        if jpl_activity:
+            jpl_activity.feedback = self.feedback
+            jpl_activity._action_done()
+            jpl_activity.write({
+                'feedback': self.feedback,
+            })
+
+        # Jika ada opportunity, cari aktivitas spesifik untuk opportunity
+        opp_activity = False
+        if self.jpl_id.opportunity_id and self.jpl_id.activity_type_id:
+            opp_activity = self.env['mail.activity'].search([
+                ('res_model_id', '=', self.env['ir.model'].search([('model', '=', 'crm.lead')], limit=1).id),
+                ('res_id', '=', self.jpl_id.opportunity_id.id),
+                ('activity_type_id', '=', self.jpl_id.activity_type_id.id)
+            ], limit=1)
+
+            # Tandai aktivitas opportunity sebagai selesai jika ditemukan
+            if opp_activity:
+                opp_activity.feedback = self.feedback
+                opp_activity._action_done()
+                opp_activity.write({
                     'feedback': self.feedback,
                 })
+
         if self.jpl_id.activity_type_id and self.jpl_id.activity_type_id.name == 'Call':
             self.jpl_id.jpl_line_state = 'finished'
             self.jpl_id.is_call = True
