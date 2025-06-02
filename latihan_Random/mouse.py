2ba43ae92dbc50a3e6313cfa2fc28f2fe65cac6a
fhnasgf28 <farhan.assegaf19019@student.unsika.ac.id>
2025-06-02 20:58:04 +0700

    mouse over

--- latihan_Random/mouse.py
+++ latihan_Random/mouse.py
@@ -1,15 +1,19 @@
 import pyautogui
 import time
+import random
 
-# Jarak gerakan mouse ke kanan (dalam pixel)
-offset = 100
+# Jarak gerakan mouse maksimum (dalam pixel)
+max_offset = 100
 
 try:
     while True:
         current_x, current_y = pyautogui.position()
-        new_x = current_x + offset
-        pyautogui.moveTo(new_x, current_y, duration=0.5)
-        print(f"Mouse moved to: ({new_x}, {current_y})")
+        offset_x = random.randint(-max_offset, max_offset)
+        offset_y = random.randint(-max_offset, max_offset)
+        new_x = current_x + offset_x
+        new_y = current_y + offset_y
+        pyautogui.moveTo(new_x, new_y, duration=0.5)
+        print(f"Mouse moved to: ({new_x}, {new_y})")
         time.sleep(60)  # tunggu 1 menit
 except KeyboardInterrupt:
     print("Program dihentikan.")
