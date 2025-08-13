73d360f204eced3c1eb0972b0b7bec0a56e35ddf
fhnasgf28 <farhan.assegaf19019@student.unsika.ac.id>
2025-08-13 12:31:35 +0700

    domain filter dan res partner

--- python_odoo/Hashmicro/res_partner.py
+++ python_odoo/Hashmicro/res_partner.py
@@ -1,6 +1,6 @@
 from odoo import fields, api, models, _
 
-class res_partner(models.Model):
+class Res_partner(models.Model):
     _inherit = 'res.partner'
 
     # default branch_id
@@ -11,11 +11,11 @@ class res_partner(models.Model):
         context = dict(self.env.context)
         if context.get('branch_id'):
             return [('branch_id', '=', context.get('branch_id'))]
-        return super(res_partner, self)._default_domain()
+        return super(Res_partner, self)._default_domain()
 
     @api.model
     def create(self, vals):
-        res = super(res_partner, self).create(vals)
+        res = super(Res_partner, self).create(vals)
         config = self.env['sales.config.settings'].search([], limit=1, order='id desc')
         approval = config.is_customer_partner_approval_matrix
         if approval and res.is_customer:
@@ -52,3 +52,30 @@ class res_partner(models.Model):
                 ]
             }
         }
+
+    def write(self, vals):
+        res = super(Res_partner, self).write(vals)
+        if 'is_customer' in vals and vals.get('is_customer'):
+            customer_group = self.env.ref('equip3_purchase_vendor_portal.group_customer_user')
+            vendor_group = self.env.ref('equip3_purchase_vendor_portal.group_vendor_user')
+            user_ids = self.user_ids and self.user_ids[0].id or False
+            if user_ids:
+                customer_group.sudo().write({
+                    'users': [(4, user_ids)]
+                })
+                vendor_group.sudo().write({
+                    'users': [(3, user_ids)]
+                })
+            elif 'is_vendor' in vals and vals.get('is_vendor'):
+                customer_group = self.env.ref('equip3_purchase_vendor_portal.group_customer_user')
+                vendor_group = self.env.ref('equip3_purchase_vendor_portal.group_vendor_user')
+                user_ids = self.user_ids and self.user_ids[0].id or False
+                if user_ids:
+                    vendor_group.sudo().write({
+                        'users': [(4, user_ids)]
+                    })
+                    customer_group.sudo().write({
+                        'users': [(3, user_ids)]
+                    })
+        return res
+
