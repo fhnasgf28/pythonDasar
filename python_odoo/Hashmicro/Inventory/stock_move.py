c132ad952b749b5ea1c2dcff9191725998c2af78
fhnasgf28 <farhan.assegaf19019@student.unsika.ac.id>
2025-10-17 21:42:25 +0700

    override method sh_stock_move_barcode_mobile_has_tracking

--- python_odoo/Hashmicro/Inventory/stock_move.py
+++ python_odoo/Hashmicro/Inventory/stock_move.py
@@ -1,4 +1,5 @@
-from odoo import models, fields, api
+from odoo import models, fields, api, _
+from odoo.exceptions import UserError
 
 
 class StockMove(models.Model):
@@ -20,3 +21,15 @@ class StockMove(models.Model):
         for move in moves_with_height:
             move.height = (move.product_qty * move.product_id.height)
         (self - moves_with_height).height = 0
+
+    def sh_stock_move_barcode_mobile_has_tracking(self, CODE_SOUND_SUCCESS, CODE_SOUND_FAIL):
+        barcode = getattr(self, 'sh_stock_move_barcode_mobile', False)
+        if barcode and self.picking_code == 'incoming' and self.product_id and self.product_id.tracking in ('lot', 'serial'):
+            existing_lot = self.env['stock.production.lot'].search([
+                ('name', '=', barcode),
+                ('product_id', '=', self.product_id.id)
+            ], limit=1)
+            if existing_lot:
+                raise UserError(_("Lot %s already exists") % barcode)
+
+        return super(StockMove, self).sh_stock_move_barcode_mobile_has_tracking(CODE_SOUND_SUCCESS, CODE_SOUND_FAIL)
