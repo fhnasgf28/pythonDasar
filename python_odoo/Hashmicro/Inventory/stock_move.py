52901864baef1ffdf841121e6ca667c66383c9d3
fhnasgf28 <farhan.assegaf19019@student.unsika.ac.id>
2025-10-17 21:19:42 +0700

    menambahkan field width dan height

--- python_odoo/Hashmicro/Inventory/stock_move.py
+++ python_odoo/Hashmicro/Inventory/stock_move.py
@@ -4,10 +4,13 @@ from odoo import models, fields, api
 class StockMove(models.Model):
     _inherit = 'stock.move'
 
+    width = fields.Float(string='Width', compute='_cal_move_width')
+    height = fields.Float(string='Height', compute='_cal_move_height')
+
     @api.depends('product_id', 'product_uom_qty', 'product_uom')
     def _cal_move_width(self):
         moves_with_width = self.filtered(lambda moves: moves.product_id.width > 0.00)
-        for move in moves_with_width:
+        for move in moves_with_width:   
             move.width = (move.product_qty * move.product_id.width)
         (self - moves_with_width).width = 0
 
