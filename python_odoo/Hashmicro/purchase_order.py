a8c3d19ab59249ad0e489a0ff73c7f58804933a8
fhnasgf28 <farhan.assegaf19019@student.unsika.ac.id>
2025-12-02 15:41:49 +0700

    purchase_order.py, check_expected_date,  onchange_partner_id, button_on_hold_confirm

--- python_odoo/Hashmicro/purchase_order.py
+++ python_odoo/Hashmicro/purchase_order.py
@@ -1,5 +1,7 @@
 from odoo import models, fields, api
 from odoo.exceptions import ValidationError
+from datetime import datetime
+from odoo.tools import DEFAULT_SERVER_DATE_FORMAT
 
 class PurchaseOrder(models.Model):
     _inherit = 'purchase.order'
@@ -42,3 +44,36 @@ class PurchaseOrder(models.Model):
             'res_model': 'approval.reject',
             'target': 'new'
         }
+
+    def _compute_hold_purchase_order(self):
+        for record in self:
+            if self.env.user.has_group('equip3_purchase_operation.group_purchase_order_partner_credit_limit'):
+                record.is_hold_purchase_order = True
+            else:
+                record.is_hold_purchase_order = False
+
+    def button_on_hold_confirm(self):
+        self.state = 'sent'
+        self.with_context(order_confirm=True).button_confirm()
+
+
+    @api.onchange('partner_id', 'company_id')
+    def onchange_partner_id(self):
+        res = super(PurchaseOrder,self).onchange_partner_id()
+        self._compute_hold_purchase_order()
+        return res
+
+    def check_expected_date(self):
+        params = {'order_id':self.id}
+        for rec in self.order_line.filtered(lambda x:not x.display_type):
+            seller = rec.product_id._select_seller_expected_date(
+                partner_id = rec.partner_id,
+                quantity=rec.product_qty,
+                date=datetime.now(),
+                uom_id=rec.product_uom,
+                params=params
+            )
+            planned_date = rec._get_date_planed(seller) if seller else datetime.now()
+            planned_date = planned_date.date()
+            planned_datetime = datetime.now().replace(year=planned_date.year, month=planned_date.month, day=planned_date.day)
+            rec.date_planed = planned_datetime.strftime(DEFAULT_SERVER_DATE_FORMAT)
