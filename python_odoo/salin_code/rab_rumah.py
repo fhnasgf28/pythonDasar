2c87171c0f697b31140fb00e10b473dbc82983dc
fhnasgf28 <farhan.assegaf19019@student.unsika.ac.id>
2025-12-20 00:08:55 +0700

    latihan,RAB Rumah 1

--- python_odoo/salin_code/rab_rumah.py
+++ python_odoo/salin_code/rab_rumah.py
@@ -21,5 +21,33 @@ class Material:
         return self.harga_satuan * self.jumlah
 
     def __str__(self):
-        return f""
+        return f"{self.nama} ({self.satuan}) @ {format_rupiah(self.harga_satuan)}"
 
+class Pekerja:
+    def __init__(self, peran, gaji_harian, durasi_kerja):
+        self.peran = peran
+        self.gaji_harian = gaji_harian
+        self.durasi_kerja = durasi_kerja
+
+    def hitung_gaji_total(self):
+        return self.gaji_harian * self.durasi_kerja
+
+    def __str__(self):
+        return f"{self.peran} ({self.durasi_kerja} hari) @ {format_rupiah(self.gaji_harian)}/hari"
+
+
+# class proyek rumah (main controller)
+class ProyekRumah:
+    def __init__(self, nama_proyek, budget_limit, **detail_tambahan):
+        self.nama_proyek = nama_proyek
+        self.budget_limit = budget_limit
+        self.detail = detail_tambahan
+        self.list_material = []
+        self.list_pekerja = []
+
+    def tambah_material(self, *args):
+        for item in args:
+            if isinstance(item, Material):
+                self.list_material.append(item)
+            else:
+                print(item)
