bd03cc55216cb3d9b40f4c7ef4d4d27e3f376c70
fhnasgf28 <farhan.assegaf19019@student.unsika.ac.id>
2026-01-22 16:00:34 +0700

    latihan, bi product pack

--- python_odoo/salin_code/bi_product_pack.py
+++ python_odoo/salin_code/bi_product_pack.py
@@ -1,4 +1,4 @@
-from odoo import models, api, _
+from odoo import models, api, _, fields
 from odoo.exceptions import ValidationError
 
 class BiProductPack(models.Model):
@@ -7,6 +7,8 @@ class BiProductPack(models.Model):
 
     bi_product_template = fields.Many2one('product.template', required=True, ondelete='cascade')
     product_id = fields.Many2one('product.product', required=True)
+    proportion = fields.Float(string='Proportion(%)')
+    product_template = fields.Many2one('product.template', string='Product', domain=[('type', '=', 'product')])
 
     @api.constrains('product_id', 'bi_product_template')
     def _check_duplicate_product_in_pack(self):
@@ -23,4 +25,19 @@ class BiProductPack(models.Model):
                 raise ValidationError(
                     _("product '%s' already exists in this bundle please choose a different product")
                     % line.product_id.display_name
-                ) 
+                )
+
+    @api.onchange('product_template')
+    def _onchange_product_template(self):
+        if self.product_template:
+            self.product_id = self.env['product.product'].search([('product_tmpl_id', '=', self.product_template.id)], limit=1)
+
+    @api.model
+    def _initial_stock_bundle(self):
+        parent_bundles = self.env['product.product'].search(
+            [('is_pack', '=', True), ('bi_pack_ids', '!=', False)]
+        )
+        warehouses = self.env['stock.warehouse'].search([])
+        existing_records = self.env['bundle.stock'].search_read(
+
+        )
