00e83fe9325c71b9b8b684522073afc4905ca6a5
fhnasgf28 <farhan.assegaf19019@student.unsika.ac.id>
2026-03-15 23:21:13 +0700

    menambahkan compute

--- latihan_Random/odoo19/crm_leads.py
+++ latihan_Random/odoo19/crm_leads.py
@@ -17,3 +17,24 @@ class CrmLead(models.Model):
             team = self.env['crm.team']._get_default_team_id(user.id, team_domain)
             if lead.team_id != team:
                 lead.team_id = team.id 
+    
+    @api.depends('team_id', 'type')
+    def _compute_stage_id(self):
+        for lead in self:
+            if not lead.stage_id or (lead.team_id and lead.stage_id.team_ids and lead.team_id not in lead.stage_id.team_ids):
+                lead.stage_id = lead._stage_find(domain=[('fold', '=', False)]).id 
+    
+    @api.depends('user_id')
+    def _compute_date_open(self):
+        for lead in self:
+            if not lead.date_open and lead.user_id:
+                lead.date_open = self.env.cr.now()
+    
+    @api.depends('stage_id')
+    def _compute_date_last_stage_update(self):
+        for lead in self:
+            if not lead.date_last_stage_update:
+                lead.date_last_stage_update = self.env.cr.now()
+
+    @api.depends('partner_id')
+    def _compute_function(self):
